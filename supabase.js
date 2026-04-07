57e501979fc5b2d296b76d9f777647fbc7fe11f8
ovidiuspeo <ovidiu_dolcan@yahoo.com>
2026-04-07 23:49:03 +0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://uymmflfhpeurfiigeivh.supabase.co"
+const SUPABASE_KEY = "PUBLIC-ANON-KEY-AICI"
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
