b7292e8a9b1d17979e2bc39fa18005534a0a4989
ovidiuspeo <ovidiu_dolcan@yahoo.com>
2026-05-02 10:04:58 +0300

    Update listare.js

--- listare.js
+++ listare.js
@@ -8,7 +8,7 @@ async function listPesteriByBazin(codBazin) {
             Var,
             Denumire,
             DomMorf,
-            Nr Desc,
+            NrDesc,
             AltAbs,
             AltRel,
             DezvTot,
