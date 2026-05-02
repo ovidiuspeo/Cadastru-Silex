169a1f11f81f3ac495dc19f42be6109d3325621d
ovidiuspeo <ovidiu_dolcan@yahoo.com>
2026-05-02 22:17:53 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,6 @@
-const SUPABASE_URL = "https://uymmflfhpeurfiigeivh.supabase.co";
-const SUPABASE_KEY = "sb_publishable_XQjrq5LLhA4b3O32ioObqQ_PNbJD5E5";
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+if (typeof supabase === 'undefined') {
+    const SUPABASE_URL = "https://uymmflfhpeurfiigeivh.supabase.co";
+    const SUPABASE_KEY = "sb_publishable_XQjrq5LLhA4b3O32ioObqQ_PNbJD5E5";
+    window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+    var supabase = window.supabaseClient; 
+}
