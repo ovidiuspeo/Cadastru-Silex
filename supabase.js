5614577bc475f62838858ada26bd623acb68bb1c
ovidiuspeo <ovidiu_dolcan@yahoo.com>
2026-05-02 22:47:41 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,11 @@
-if (typeof supabase === 'undefined') {
-    const SUPABASE_URL = "https://uymmflfhpeurfiigeivh.supabase.co";
-    const SUPABASE_KEY = "sb_publishable_XQjrq5LLhA4b3O32ioObqQ_PNbJD5E5";
-    window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-    var supabase = window.supabaseClient; 
+console.log("Se inițializează Supabase...");
+
+const SUPABASE_URL = "https://supabase.co";
+const SUPABASE_KEY = "sb_publishable_XQjrq5LLhA4b3O32ioObqQ_PNbJD5E5";
+
+try {
+    window.db_supa = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+    console.log("Conexiune creată cu succes în window.db_supa");
+} catch (e) {
+    console.error("Eroare la crearea clientului Supabase:", e);
 }
