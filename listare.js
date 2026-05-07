9360cc5066c75c0146353343856257cc8ba41fa0
ovidiuspeo <ovidiu_dolcan@yahoo.com>
2026-05-07 15:19:28 +0300

    Update listare.js

--- listare.js
+++ listare.js
@@ -1,4 +1,4 @@
-// 1. Funcția care afișează tabelul (ACTUALIZATĂ pentru link-uri) v.1.10
+// 1. Funcția care afișează tabelul (ACTUALIZATĂ pentru link-uri) v.1.11
 function renderTable(rows, coloaneDeAfisat) {
     const theadRow = document.querySelector("#tabelPesteri thead tr");
     const tbody = document.querySelector("#tabelPesteri tbody");
